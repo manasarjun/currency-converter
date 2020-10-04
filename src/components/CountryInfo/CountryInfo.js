2a2dd57e71c3d7efbb4fa8ef8dabe0f5c7c20684
manasarjun <manasakanthala@gmail.com>
2020-10-04 17:48:55 +0200

    feat: Add Country info

---
+++ src/components/CountryInfo/CountryInfo.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './CountryInfo.css'
+
+function CountryInfo({ country }) {
+
+  const currencies = country['currencies'];
+
+  const currencyName = currencies.map(
+    (currency, i) => (
+      <span className='acceptedCurrency' key={i}>
+        <ul>
+          <li>{currency.name}</li>
+          <br />
+        </ul>
+      </span>
+    ));
+
+
+  const { name, capital, population } = country;
+  return (
+    <>
+      <div className='container'>
+        <p>Country Name: {name} </p>
+        <p>Capital: {capital} </p>
+        <p>Population: {population}</p>
+        <p>Accepted Currency: </p>{currencyName}
+      </div>
+    </>
+  )
+
+}
+
+CountryInfo.propTypes = {
+
+}
+
+export default CountryInfo
+
